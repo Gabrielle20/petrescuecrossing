59e3f91ddee136395778482b7170458f82a28493
Lily <lilybelhocine006@gmail.com>
2021-12-22 18:22:17 +0100

    Add donation page

---
+++ src/Controller/DonsController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\PostType;
+use App\Entity\Dons;
+use App\Repository\UserRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+
+class DonsController extends AbstractController
+{
+    /**
+     * @Route("/dons", name="dons")
+     */
+    public function index(Request $request, ManagerRegistry $mr, UserRepository $userRepository): Response
+    {
+        $data = $this -> getData();
+        $don = new Dons();
+        $form = $this->createForm(PostType::class, $don);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+
+            $montant = $form-> getData() -> getMontant();
+            $this ->addFlash("montant", $montant);
+            return $this->redirectToRoute("paiement");
+        }
+
+        return $this->render('dons/index.html.twig', [
+            'form' => $form ->createView(),
+            'montant' => $data
+        ]);
+    }
+
+    /**
+     * @Route("/dons/paiement", name="paiement")
+     */
+    public function processPaiement(Request $request, ManagerRegistry $mr, UserRepository $userRepository)
+    {
+        $montant = $this->get('session')->getFlashBag()->get('montant');
+        $dataMontant= isset($montant[0]) ? $montant[0] : $montant;
+
+        $form = $this -> createFormBuilder()->add('name', TextType::class)
+            ->add('NumCarte', NumberType::class)
+            ->add('Date', DateType::class)
+            ->add('cvv', NumberType::class)
+            ->add('Valider', SubmitType::class)
+            ->add('montant', HiddenType::class, ["data" => $dataMontant])
+            ->getForm();
+        $form -> handleRequest($request);
+
+        if($form -> isSubmitted() && $form ->isValid()){
+            $don = new Dons();
+
+            $User= $userRepository ->find(1);
+            $don -> setMontant(intval($form-> getViewData()["montant"]));
+            $don -> setUser($User);
+            $don->setDate(new \DateTime());
+
+            $em = $mr->getManager();
+            $em->persist($don);
+            $em->flush();
+
+            $this->addFlash("success", "Don bien enregistré");
+            return $this->redirectToRoute("dons");
+        }
+        return $this->render('dons/paiement.html.twig', [
+            'form' => $form ->createView(),
+        ]);
+
+
+    }
+
+    public function getData()
+    {
+        $data = $this->getDoctrine()->getRepository(Dons::class)->countMontant();
+        return $data[0][1];
+
+    }
+}
---
+++ src/Form/FormDon.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Dons;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PostType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $choices = $this->createChoices();
+        $builder
+            ->add('montant', NumberType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Dons::class,
+        ]);
+    }
+
+    public function createChoices()
+    {
+        $choices = array();
+        for ($i=0; $i < 10; $i++) {
+            $choices[] = $i;
+        }
+        return $choices;
+    }
+}
+
